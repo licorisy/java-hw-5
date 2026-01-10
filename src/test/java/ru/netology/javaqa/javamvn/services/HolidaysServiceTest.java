387032bf7fffbbc9ab8c3c41e58f9465e4fb0bcc
licoris <klepehin.k.a@gmail.com>
2026-01-10 18:37:07 +0300

    csv

--- src/test/java/ru/netology/javaqa/javamvn/services/HolidaysServiceTest.java
+++ src/test/java/ru/netology/javaqa/javamvn/services/HolidaysServiceTest.java
@@ -2,27 +2,32 @@ package ru.netology.javaqa.javamvn.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class HolidaysServiceTest {
 
-    @Test
-    public void SmallMoney() {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+
+    public void SmallMoney(int expected, int income, int expenses, int threshold) {
         HolidaysService service = new HolidaysService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        Assertions.assertEquals(3, service.calculate(10_000, 3_000, 20_000));
+        Assertions.assertEquals(expected, service.calculate(income, expenses, threshold));
     }
 
-    @Test
-    public void BigMoney() {
-        HolidaysService service = new HolidaysService();
+    // @Test
+    //  public void BigMoney() {
+    //      HolidaysService service = new HolidaysService();
 
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        Assertions.assertEquals(2, service.calculate(100_000, 60_000, 150_000));
-    }
-
+    //      int income = 100_000;
+    //     int expenses = 60_000;
+    //     int threshold = 150_000;
+//Assertions.assertEquals(2, service.calculate(100_000, 60_000, 150_000));
 }
+
+
