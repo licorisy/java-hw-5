be99f0bf10820d381e9c46ab5192c4c037c5d3b2
licoris <klepehin.k.a@gmail.com>
2026-01-12 15:52:27 +0300

    new

--- src/main/java/ru/netology/javaqa/javamvn/services/HolidaysService.java
+++ src/main/java/ru/netology/javaqa/javamvn/services/HolidaysService.java
@@ -2,18 +2,18 @@ package ru.netology.javaqa.javamvn.services;
 
 public class HolidaysService {
     public int calculate(int income, int expenses, int threshold) {
-        int Count = 0;       // счётчик месяцев отдыха
-        int Money = 0;          // сумма денег на счету
+        int count = 0;       // счётчик месяцев отдыха
+        int money = 0;          // сумма денег на счету
         for (int month = 0; month < 12; month++) {
-            if (Money >= threshold) {           // проверяем, достаточно ли денег для отдыха
-                Money -= expenses;              // сначала оплачиваем обычные расходы
-                Money /= 3;                     // затем оставшиеся средства сокращаем втрое
-                Count++;
+            if (money >= threshold) {           // проверяем, достаточно ли денег для отдыха
+                money -= expenses;              // сначала оплачиваем обычные расходы
+                money /= 3;                     // затем оставшиеся средства сокращаем втрое
+                count++;
             } else {
-                Money += income - expenses;     // работаем и получаем разницу между заработком и расходами
+                money += income - expenses;     // работаем и получаем разницу между заработком и расходами
             }
         }
 
-        return Count;
+        return count;
     }
 }
